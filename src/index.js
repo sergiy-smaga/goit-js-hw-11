c45957168531126e01246e127ffa93a6958edee7
sergiy-smaga <seregasmaga@gmail.com>
2022-06-15 20:13:47 +0300

    2nd

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
-import { Notify } from 'notiflix/build/notiflix-notify-aio';
-import SimpleLightbox from 'simplelightbox/dist/simple-lightbox.esm';
-const axios = require('axios').default;
+// import { Notify } from 'notiflix/build/notiflix-notify-aio';
+// import SimpleLightbox from 'simplelightbox/dist/simple-lightbox.esm';
+// const axios = require('axios').default;
